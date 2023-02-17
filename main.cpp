be8183c6582e1470298b2efa373652228b246746
jaseyaws <jcartin@ykw.systems>
2023-02-17 07:47:24 -0500

    start of main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+using FText = std::string;
+using int32 = int;
+
+
+void PrintIntro();
+void PlayGame();
+FText GetValidGuess();
+bool AskToPlayAgain();
+void PrintGameSummary();
