c650f6681b46e49e1751046a9a03dd6dc8e4ab38
jaseyaws <jcartin@ykw.systems>
2023-02-17 07:51:04 -0500

    add header file

---
+++ FBullCowGame.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+
+class FBullCowGame {
+public:
+	FBullCowGame();
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
+
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
+
+private:
+	int32 MyCurrentTry;
+	int32 MyMaxTries;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+
+	bool IsIsogram(FString) const;
+};
